1a0c87042b48408e8c41fdfab9e542225bccae00
Nikita Grishin <nikita.grishin@revolut.com>
2020-04-11 16:53:38 +0300

    format

--- auth/src/crypto.ts
+++ auth/src/crypto.ts
@@ -10,7 +10,7 @@ const key = new NodeRSA()
 try {
   const PRIVATE_RSA_KEY_BASE64: string = process.env.PRIVATE_RSA_KEY as string
   const PUBLIC_RSA_KEY_BASE64: string = process.env.PUBLIC_RSA_KEY as string
-  
+
   key.importKey(
     Buffer.from(PUBLIC_RSA_KEY_BASE64, 'base64').toString('utf-8'),
     'public'
@@ -18,14 +18,16 @@ try {
   key.importKey(
     Buffer.from(PRIVATE_RSA_KEY_BASE64, 'base64').toString('utf-8'),
     'private'
-  ) 
-} catch(e) {
+  )
+} catch (e) {
   console.error('RUNNING IN AN UNECRYPTED MODE!!!')
   ENCRYPT = false
 }
 
-export const encrypt = (data: string) => ENCRYPT ? key.encrypt(data, 'base64') : data
-export const decrypt = (data: string) => ENCRYPT ? key.decrypt(data).toString('utf-8') : data
+export const encrypt = (data: string) =>
+  ENCRYPT ? key.encrypt(data, 'base64') : data
+export const decrypt = (data: string) =>
+  ENCRYPT ? key.decrypt(data).toString('utf-8') : data
 
 export const generateSalt = () =>
   crypto
